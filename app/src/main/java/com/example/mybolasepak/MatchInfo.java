6e2d69a82f8a4530e81d6300bb1c76e53752d907
naufalzhafran <latifnaufal7@gmail.com>
2020-03-12 15:00:49 +0700

    Create better event detail

---
+++ app/src/main/java/com/example/mybolasepak/MatchInfo.java
@@ -0,0 +1,28 @@
+package com.example.mybolasepak;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MatchInfo extends Fragment {
+
+    public MatchInfo() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_match_info, container, false);
+    }
+}
