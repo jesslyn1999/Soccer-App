594065e76f4251f408b00ea82c40f81507befebb
Anzaldi Sulaiman Oemar <anzaldy@gmail.com>
2020-03-13 23:21:13 +0700

    Add parameter retrofit

--- app/src/main/java/com/example/mybolasepak/api/RetrofitServiceInterface.java
+++ app/src/main/java/com/example/mybolasepak/api/RetrofitServiceInterface.java
@@ -1,21 +1,24 @@
 package com.example.mybolasepak.api;
 
 import com.example.mybolasepak.model.EventList;
+import com.example.mybolasepak.model.Event;
 import com.example.mybolasepak.model.TeamList;
+import com.example.mybolasepak.model.Team;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface RetrofitServiceInterface {
-    @GET("v1/json/1/lookupevent.php?id=441613")
-    Call<EventList> getEventListById();
+    @GET("v1/json/1/lookupevent.php")
+    Call<EventList> getEventById(@Query("id") String eventID);
 
-    @GET("v1/json/1/lookupteam.php?id=133604")
-    Call<TeamList> getTeamListById();
+    @GET("v1/json/1/lookupteam.php")
+    Call<Team> getTeamById(@Query("id") String teamID);
 
-    @GET("v1/json/1/eventsnext.php?id=133602")
-    Call<EventList> getNext5EventListByTeamId();
+    @GET("v1/json/1/eventsnext.php")
+    Call<EventList> getNext5EventsByTeamId(@Query("id") String teamID);
 
-    @GET("v1/json/1/eventslast.php?id=133602")
-    Call<EventList> getLast5EventListByTeamId();
+    @GET("v1/json/1/eventslast.php")
+    Call<EventList> getLast5EventsByTeamId(@Query("id") String teamID);
 }
